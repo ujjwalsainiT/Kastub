29245165b497776e81aa766fa48dd66e2a446628
ujjwalsainiT <ujjwalreactjsdeveloper@gmail.com>
2021-10-19 10:44:18 -0700

    ujjwal

--- src/utils/Validation.jsx
+++ src/utils/Validation.jsx
@@ -9,6 +9,8 @@ export const blankValidator = (str) => {
   }
   return true;
 };
+
+//for email Validation
 export const emailValidator = (email) => {
   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (!email.match(regEx)) {
